6f255594ee06ebe9e18ee715b47a6c258e0f0765
E66666666 <emily66666666ok@outlook.com>
2021-07-05 01:57:55 +0200

    Bump version to 2.0.0
    Yeetures

--- GhostReplay/Constants.hpp
+++ GhostReplay/Constants.hpp
@@ -2,9 +2,9 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 3
-#define VERSION_PATCH 1
+#define VERSION_MAJOR 2
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
 
 namespace Constants {
     static const char* const DisplayVersion = "v" STR(VERSION_MAJOR) "."  STR(VERSION_MINOR) "." STR(VERSION_PATCH);

5cc649240cc85e15629db3f887cd8421ef67b5ea
E66666666 <6639510+E66666666@users.noreply.github.com>
2021-07-26 01:05:00 +0200

    Warn about missing siren audio

--- GhostReplay/ReplayMenu.cpp
+++ GhostReplay/ReplayMenu.cpp
@@ -844,8 +844,9 @@ std::vector<CScriptMenu<CReplayScript>::CSubmenu> GhostReplay::BuildMenu() {
         mbCtx.BoolOption("Record indicators", GetSettings().Record.Optional.Indicators,
             { "Records indicators (left, right and hazard) when enabled." });
 
-        mbCtx.BoolOption("Record siren", GetSettings().Record.Optional.Siren,
-            { "Records siren when enabled. Only records on/off, doesn't record fast/slow." });
+        mbCtx.BoolOption("Record emergency lights/siren", GetSettings().Record.Optional.Siren,
+            { "Records emergency lights/siren when enabled. Only records on/off, doesn't record fast/slow.",
+              "Keep in mind the siren is only audible if the playback vehicle has a driver." });
 
         mbCtx.BoolOption("Reduce file size", GetSettings().Record.ReduceFileSize,
             { "Check to save smaller files. Uncheck to save formatted json file, e.g. for inspecting data.",
